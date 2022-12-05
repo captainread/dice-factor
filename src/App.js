bdbf9f07b13b7dc7e312d2646f6a36b20b542eb6
Alice Read <aread773@gmail.com>
2022-12-05 16:26:16 +0000

    MUI setup and navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import "./index.css";
+// import { useState } from "react";
+import { BrowserRouter } from "react-router-dom";
+// import { UserContext } from "./components/contexts";
+import * as React from "react";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import Nav from "./components/Nav";
+import { appTheme } from "./themes/theme";
 
 function App() {
+  // const [user, setUser] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header"></header>
-    </div>
+    <BrowserRouter>
+      <React.Fragment>
+        <ThemeProvider theme={appTheme}>
+          <CssBaseline enableColorScheme />
+          {/* <UserContext.Provider value={{ user, setUser }}> */}
+          <div className="App">
+            <header className="App-header"></header>
+            <Nav />
+          </div>
+          {/* </UserContext.Provider> */}
+        </ThemeProvider>
+      </React.Fragment>
+    </BrowserRouter>
   );
 }
 
---
+++ src/themes/theme.js
@@ -0,0 +1,24 @@
+import { createTheme } from "@mui/material/styles";
+
+export const appTheme = createTheme({
+  palette: {
+    primary: {
+      main: "#45cbb2",
+    },
+    secondary: {
+      light: "#0066ff",
+      main: "#45cbb2",
+      contrastText: "#3e2f34",
+    },
+
+    custom: {
+      light: "#ffa726",
+      main: "#f57c00",
+      dark: "#ef6c00",
+      contrastText: "rgba(0, 0, 0, 0.87)",
+    },
+
+    contrastThreshold: 3,
+    tonalOffset: 0.2,
+  },
+});
